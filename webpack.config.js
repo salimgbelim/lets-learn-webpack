387a40851962de6607a052183f9044c5ef96101e
MSMSalim <salim.belim@moneysupermarket.com>
2017-12-28 13:21:16 +0000

    Salim:working css,scss and babel loaders with webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {loader: "style-loader"},
+                    {loader: "css-loader"},
+                    {loader: "sass-loader"}
+                ]
+            },
+            {
+                test: /\.js/,
+                exclude: /node_modules/,
+                use: [
+                    {loader: "babel-loader"}
+                ]
+            }
+        ]
+    }
+}
